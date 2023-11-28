234b418142db106bfac732b24de5cd1def4c877a
Jia Rong Gan <jiaronggan21@gmail.com>
2023-11-27 20:13:44 -0600

    completed number 11 and 12

--- 11_flip_case.py
+++ 11_flip_case.py
@@ -11,3 +11,15 @@ def flip_case(phrase, to_swap):
         'AaaaHHH'
 
     """
+
+    # iterate through the phrase
+    # if the character in phrase matches the to_swap
+    # we will change character to the opposite case using method swapcase()
+    new_swap_phrase = ""
+    for char in phrase :
+        if char.lower() == to_swap.lower() :
+            new_swap_phrase += char.swapcase()
+        else :
+            new_swap_phrase += char
+
+    return new_swap_phrase
--- 12_multiply_even_numbers.py
+++ 12_multiply_even_numbers.py
@@ -1,14 +1,25 @@
 def multiply_even_numbers(nums):
     """Multiply the even numbers.
-    
+
         >>> multiply_even_numbers([2, 3, 4, 5, 6])
         48
-        
+
         >>> multiply_even_numbers([3, 4, 5])
         4
-        
+
     If there are no even numbers, return 1.
-    
+
         >>> multiply_even_numbers([1, 3, 5])
         1
     """
+    import functools
+    even_numbers = [num for num in nums if num % 2 == 0]
+
+    if even_numbers :
+        return functools.reduce(lambda a, b : a * b, even_numbers)
+    else :
+        return 1
+
+
+
+
